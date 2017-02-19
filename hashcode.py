a2e5b6c471ff5295172b25702cfa34f68cb13eb6
PelayoChoya <100353977@alumnos.uc3m.es>
2017-02-19 14:07:24 +0100

    reading files

--- hashcode.py
+++ hashcode.py
@@ -1,1 +1,6 @@
-print "hehe"
+with open("small.in", "r") as input_file:
+	first_line = input_file.readline()
+	whole = input_file.read()
+first_arr = first_line.split()
+arr = [[0 for i in range(int(first_arr[0]))] for j in range(int(first_arr[1]))]
+print whole.split()
