bf8ff9b58924116612038e74a5948b5fe13fb7a1
PelayoChoya <100353977@alumnos.uc3m.es>
2017-02-19 14:50:19 +0100

    converted to function

--- hashcode.py
+++ hashcode.py
@@ -1,15 +1,19 @@
-with open("small.in", "r") as input_file:
-	first_line = input_file.readline()
-	whole = input_file.read()
-first_arr = first_line.split()
-arr = [[0 for i in range(int(first_arr[1]))] for j in range(int(first_arr[0]))]
-whole_arr = whole.split()
-j_count = 0
-i_count = 0
-for i in whole_arr:
-	for j in i:
-		arr[i_count-1][j_count-1] = j
-		j_count += 1
+def read_file(title):
+	with open(title, "r") as input_file:
+		first_line = input_file.readline()
+		whole = input_file.read()
+	first_arr = first_line.split()
+	arr = [[0 for i in range(int(first_arr[1]))] for j in range(int(first_arr[0]))]
+	whole_arr = whole.split()
 	j_count = 0
-	i_count += 1
-print arr
+	i_count = 0
+	for i in whole_arr:
+		for j in i:
+			arr[i_count-1][j_count-1] = j
+			j_count += 1
+		j_count = 0
+		i_count += 1
+	return arr
+
+pizza = read_file("small.in")
+print pizza
